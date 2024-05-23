165c476eb41b9cbaaa8c4d6df1dd5fbc76764381
Cláudio Silva <claujs@gmail.com>
2024-05-23 19:56:13 -0300

    Create: Page to add cities on app cache

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:weather_app_mobile/src/features/weather/presentation/weather_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:weather_app_mobile/src/features/weather/presentation/weather_choose_cities.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  List<String> defaultCities = [
+    'Silverstone, UK',
+    'Sao Paulo, Brazil',
+    'Melbourne, Australia',
+    'Monte Carlo, Monaco'
+  ];
+  List<String>? savedCities = prefs.getStringList('favoriteCities');
+  if (savedCities == null || savedCities.isEmpty) {
+    prefs.setStringList('favoriteCities', defaultCities);
+  } else {
+    // Add default cities only if they're not already present
+    for (final defaultCity in defaultCities) {
+      if (!savedCities.contains(defaultCity)) {
+        savedCities.add(defaultCity);
+      }
+    }
+    prefs.setStringList('favoriteCities', savedCities);
+  }
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -11,13 +31,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Rock Weather App',
       debugShowCheckedModeBanner: false,
       theme: _buildTheme(context),
-      home: const WeatherPage(city: 'Melbourne'),
+      home: const AddCityScreen(),
     );
   }
 
--- lib/src/features/weather/application/providers.dart
+++ lib/src/features/weather/application/providers.dart
@@ -4,20 +4,27 @@ import 'package:weather_app_mobile/src/features/weather/domain/forecast/forecast
 import 'package:weather_app_mobile/src/features/weather/domain/weather/weather_data.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-final cityProvider = StateProvider<String>((ref) => 'Melbourne');
+final cityProvider = StateProvider<String>((ref) {
+  final favoriteCities = ref.watch(favoriteCitiesProvider);
+  if (favoriteCities.isNotEmpty) {
+    return favoriteCities.first; // Use the first city from the cache
+  } else {
+    return 'Melbourne'; // Default to 'Melbourne' if the cache is empty
+  }
+});
 
 final weatherProvider = FutureProvider.autoDispose<WeatherData>((ref) async {
   final city = ref.watch(cityProvider);
   final weatherResponse =
       await ref.watch(weatherRepositoryProvider).getWeather(city: city);
-  return WeatherData.from(weatherResponse!);
+  return WeatherData.from(weatherResponse);
 });
 
 final forecastProvider = FutureProvider.autoDispose<ForecastData>((ref) async {
   final city = ref.watch(cityProvider);
   final forecastResponse =
       await ref.watch(weatherRepositoryProvider).getForecast(city: city);
-  return ForecastData.from(forecastResponse!);
+  return ForecastData.from(forecastResponse);
 });
 
 final favoriteCitiesProvider = StateProvider<List<String>>((ref) => []);
